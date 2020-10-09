86c73c54548820353424db2fcb10e74cb330d06e
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-09 11:48:32 -0400

    additonal solve optionif methods are allowed

--- 05-02-max-num/max-num.js
+++ 05-02-max-num/max-num.js
@@ -4,8 +4,12 @@
 // return the last index in array? 
 
 var maxNum = function(arr) {
-    arr.sort(function(a,b){return a-b}); // array sorted ascending 
-    let highestNumIndex = arr.length - 1; // brute force solution
+    // arr.sort(function(a,b){return a-b}); // array sorted ascending 
+    // let highestNumIndex = arr.length - 1; // brute force solution
 
-    return arr[highestNumIndex];
+    // return arr[highestNumIndex];
+
+    return Math.max(...arr); 
+        // performs the static method max() of Math to determine the max number in a spread array
+        // *only used because the array is small 
 };
