77d5b2740079bef1f5961108b8aa29fff89863eb
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-09 10:57:50 -0400

    ceasar cipher to unit 21

---
+++ 21-04-caesar-cipher/caesar-cipher.js
@@ -0,0 +1,13 @@
+// Write a function that takes in a string and and an offset and returns a new string with each character moved by the offset
+
+var caesarCipher = function(str, offset) {
+    // var to hold new string with shifted letters
+        // letters must retain case (if character == character.toUpperCase() )
+    let ceasarString = "";
+
+    // for loop to step thorugh each letter in string
+    
+    // return new string
+    console.log(ceasarString);
+    return ceasarString;
+};
