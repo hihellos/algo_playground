07a31d36a6b1448f42037e6d672a5baacbd3bc96
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-09 09:34:27 -0400

    solved unit 04 1-3

--- 04-03-sum-array/sum-array.js
+++ 04-03-sum-array/sum-array.js
@@ -1,3 +1,15 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+let sumArray = function(arr) {
+    let result = 0; // where we begin, and store the total after each add
+
+    for (let i = 0; i < arr.length; i++) {
+        let currentNumber = arr[i]; // define the number you are at in each iteration of the loop
+        console.log("current number in arr: ", currentNumber);
+
+        result += currentNumber; // grab stored total, add current number and store new total
+    }
+
+    console.log("result: ", result);
+    return result;
+};
