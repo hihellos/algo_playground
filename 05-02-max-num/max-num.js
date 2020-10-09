9a0683e9f468fde8440c5d6381f416e94c5ffe05
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-09 11:35:10 -0400

    brute forced 05-02

--- 05-02-max-num/max-num.js
+++ 05-02-max-num/max-num.js
@@ -1,3 +1,11 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+// sort array
+// return the last index in array? 
+
+var maxNum = function(arr) {
+    arr.sort(function(a,b){return a-b}); // array sorted ascending 
+    let highestNumIndex = arr.length - 1; // brute force solution
+
+    return arr[highestNumIndex];
+};
