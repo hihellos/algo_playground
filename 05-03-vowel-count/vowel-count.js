673822bfee341b5f9a09e1e2f5b3d6bb9fab0c37
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-10 11:28:52 -0400

    wrapped up unit 5, used indexOf() here

--- 05-03-vowel-count/vowel-count.js
+++ 05-03-vowel-count/vowel-count.js
@@ -1,3 +1,19 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    // console.log(str);
+
+    let count = 0; // holds count total
+    let vowels = ["a", "e", "i", "o", "u"]; // array we need to compare letters to, increase count for match
+
+    for (var i = 0; i < str.length; i++) {
+        let letter = str[i].toLowerCase(); // convert all letters to lower so we do not miss an comps
+
+        if (vowels.indexOf(letter) !== -1) { // indexOf method on each letter to see if match exists, for every non -1, count++
+            count ++
+        }
+    }
+
+    // console.log(count);
+    return count;
+};
