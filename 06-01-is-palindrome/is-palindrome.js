cd7185ec2482ea13a4a84b578d63fe2df6122cec
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-10 12:08:45 -0400

    removed excessive logs, moved and refined comments

--- 06-01-is-palindrome/is-palindrome.js
+++ 06-01-is-palindrome/is-palindrome.js
@@ -1,27 +1,20 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
-
-var isPalindrome = function(str) {
-    // console.log(str);
-
-    // push string backwards to new array, unshift? 
+    // push string to array, reverse array
     // compare strings for match
     // return true for match
 
-    let newStr = [];
+var isPalindrome = function(str) {
+    let strArray = [];
     for (var i = 0; i < str.length; i++) {
-        // console.log(str[i]); 
-        newStr.push(str[i]);
+        strArray.push(str[i]);
     }
 
-    newStr.reverse();
-    let checkStr = newStr.join("");
-    // console.log(checkStr);
+    strArray.reverse();
+    let checkStr = strArray.join("");
 
     if (checkStr === str) {
-        // console.log("true");
         return true; // return boolean value, not string
     } else {
-        // console.log("false");
         return false;
     }
 };
