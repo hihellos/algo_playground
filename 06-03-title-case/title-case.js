fef2e855138c0c04c67414191997a41aa57c923d
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-20 09:47:12 -0400

    first letter in each word of string to cap

--- 06-03-title-case/title-case.js
+++ 06-03-title-case/title-case.js
@@ -1,3 +1,16 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    let words = str.split(" ");
+    let capWords = [];
+
+    for (var i = 0; i < words.length; i++) {
+        let word = words[i].split("");
+
+        word[0] = word[0].toUpperCase();
+
+        capWords.push(word.join(""));
+    }
+
+    return capWords.join(" ");
+};
