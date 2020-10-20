99d1fecb3b230f553b5c90ea4d1fc7f224e63ada
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-20 11:50:43 -0400

    reverse words in str

--- 07-04-reverse-words/reverse-words.js
+++ 07-04-reverse-words/reverse-words.js
@@ -1,3 +1,8 @@
 // Write code to create a function that accepts a string (`str`) and reverses the order of the words in the string
 
-var reverseWords = function(str) {};
+var reverseWords = function(str) {
+    let words = str.split(" ");
+    let reverse = words.reverse().join(" ");
+    
+    return reverse;
+};
