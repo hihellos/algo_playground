afc34b66890f02dbd64052e14d7ae5f8dd770ae9
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-20 11:08:41 -0400

    acronym builder solved

--- 07-03-acronym-builder/acronym-builder.js
+++ 07-03-acronym-builder/acronym-builder.js
@@ -1,3 +1,15 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+var acronymBuilder = function(str) {
+    let words = str.split(" ");
+    console.log(words);
+
+    let result = "";
+
+    for (var i = 0; i < words.length; i++) {
+        result += words[i].charAt(0).toUpperCase();
+    }
+
+    console.log(result);
+    return result;
+};
