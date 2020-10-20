4303e1fea3d4d6629dec35945cd36ceed1023d37
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-20 12:32:38 -0400

    reverse array in place, with excessive console logs for visualization

--- 08-02-reverse-in-place/reverse-in-place.js
+++ 08-02-reverse-in-place/reverse-in-place.js
@@ -1,4 +1,27 @@
-// Write code to return a new string with all of the characters in `str` reversed.
+// Write code to return a new array with all of the elements in `arr` reversed.
 // You may NOT use the built-in reverse method
 
-var reverseInPlace = function(arr) {};
+var reverseInPlace = function(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) { // while right is > left, meaning there are still items to the right before first item 0
+        let tempArr = arr[left]; // start of where we do a bunch of reassigning to assign the last item in the array to the front of array
+        console.log("temp arr start: ", tempArr);
+        console.log("arr left start: ", arr[left]);
+        console.log("arr right start: ", arr[right]);
+
+        arr[left] = arr[right];
+        console.log("arr left after assigned to be arr right: ", arr[left]);
+        console.log("arr right: ", arr[right]);
+        arr[right] = tempArr; // this reassignment loops back to the tempArr
+        console.log("arr right: ", arr[right]);
+        console.log("temp arr: ", tempArr);
+
+        left++;
+        right--;
+    }
+
+    // console.log(arr);
+    return arr;
+};
