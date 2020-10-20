7a727d7e6a476e435e3f1347bd0706333c4b1f6a
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-20 10:01:48 -0400

    swap letter cases in string

--- 07-01-swap-case/swap-case.js
+++ 07-01-swap-case/swap-case.js
@@ -1,3 +1,18 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+var swapCase = function(str) {
+    let result = "";
+
+    for (var i = 0; i < str.length; i++){
+        let letter = str[i];
+
+        if (letter === letter.toUpperCase()) {
+            result += letter.toLowerCase();
+        } else {
+            result += letter.toUpperCase();
+        }
+    }
+    
+    console.log(result);
+    return result;
+};
