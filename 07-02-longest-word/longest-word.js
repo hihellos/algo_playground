1e3c809a2a00572068e18381c12297b60670a728
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-20 11:03:06 -0400

    solved longest word, added extra test cases, given >1 longest word, currently returns first longest word found

--- 07-02-longest-word/longest-word.js
+++ 07-02-longest-word/longest-word.js
@@ -1,3 +1,12 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+var longestWord = function(str) {
+    let words = str.split(" ");
+
+    let longestWord = words.sort(function(a, b) {
+        return b.length - a.length;
+    })
+
+    console.log(longestWord[0]);
+    return longestWord[0];
+};
--- 07-02-longest-word/test/longest-word.test.js
+++ 07-02-longest-word/test/longest-word.test.js
@@ -16,4 +16,21 @@ describe("longestWord", function() {
 
     expect(result).to.eql("Remembers");
   });
+
+  it("should return 'otorhinolaryngology' when given 'What if we try a super-long word such as otorhinolaryngology'", function() {
+    var str = "What if we try a super-long word such as otorhinolaryngology";
+
+    var result = longestWord(str);
+
+    expect(result).to.eql("otorhinolaryngology");
+  });
+
+  it("should return 'airspeed' or 'velocity' when given 'What is the average airspeed velocity of an unladen swallow'", function() {
+    var str = "What is the average airspeed velocity of an unladen swallow";
+
+    var result = longestWord(str);
+
+    expect(result).to.eql("airspeed" || "velocity");
+  });
+
 });
