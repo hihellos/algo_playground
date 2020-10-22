cf00755f32aed3527bf537826db4e964804d440c
hihello <13111181+hihellos@users.noreply.github.com>
2020-10-22 15:52:46 -0400

    solved is unique with Set, if/else and ternary solutions

--- 10-02-is-unique/is-unique.js
+++ 10-02-is-unique/is-unique.js
@@ -1,4 +1,14 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+    let set = [...new Set(arr)];
+
+    // if (set.length === arr.length) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return set.length === arr.length ?  true : false;
+};
